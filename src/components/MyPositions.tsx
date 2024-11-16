c3644acf4845c751e4676a12d9bb175afb82a66d
ferrodri <f.rodriguez.hervias@gmail.com>
2024-11-16 18:45:37 +0700

    add todo

--- src/components/MyPositions.tsx
+++ src/components/MyPositions.tsx
@@ -1,53 +1,53 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { VaultPosition } from '@/types';
+import type { VaultPosition } from '@/types';
 import { Loading } from './Loading';
 import Position from './Position';
 
-export default function MyPositions({ 
+export default function MyPositions({
   vaults,
-  loading = true 
-}: Readonly<{ 
-  vaults: VaultPosition[], 
-  loading: boolean 
+  loading = true
+}: Readonly<{
+  vaults: VaultPosition[];
+  loading: boolean;
 }>) {
   return (
-    <div style={{minHeight: '60vh'}}
+    <div
+      style={{ minHeight: '60vh' }}
       className={cn(
         'w-full h-full border border-[#456DB5] bg-[#1B1D1F] rounded-xl grid justify-between items-center',
         'gap-8 relative',
-        !vaults || loading || (vaults.filter(e => e.assetsUsd > 0).length > 1) ? 'grid-cols-2' : 'grid-cols-1'
+        !vaults || loading || vaults.filter((e) => e.assetsUsd > 0).length > 1 ? 'grid-cols-2' : 'grid-cols-1'
       )}
     >
-      { loading ?
-        (
-          <>
-            <span className="absolute justify-self-center	"><Loading/></span>
-            {[0, 1, 2, 3].map((item) => (
-        <div key={item} className="h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl"></div>
-      ))}
-          </>
-        ) : vaults && vaults.length > 0  && vaults.some(e => e.assetsUsd > 0)? (
-          <>
+      {loading ? (
+        <>
+          <span className="absolute justify-self-center	">
+            <Loading />
+          </span>
+          {[0, 1, 2, 3].map((item) => (
+            <div key={item} className="h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl"></div>
+          ))}
+        </>
+      ) : vaults && vaults.length > 0 && vaults.some((e) => e.assetsUsd > 0) ? (
+        <>
           {vaults.map((item) => {
             if (item.assetsUsd > 0) {
-              return <Position key={item.vault.address} vaultPosition={item}/>
+              return <Position key={item.vault.address} vaultPosition={item} />;
             }
           })}
-          </>
-        ) : (
-          <>
-                <span className="absolute text-center text-[#355180] text-5xl px-32 leading-loose">
-        You do not currently have any open positions to manage.
-      </span>
-      {[0, 1, 2, 3].map((item) => (
-        <div key={item} className="h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl"></div>
-      ))}
-          </>
-        )
-      }
-
+        </>
+      ) : (
+        <>
+          <span className="absolute text-center text-[#355180] text-5xl px-32 leading-loose">
+            You do not currently have any open positions to manage.
+          </span>
+          {[0, 1, 2, 3].map((item) => (
+            <div key={item} className="h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl"></div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
