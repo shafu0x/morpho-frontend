87cafec74d413a6273ab80b61330259a861f2303
meirbank <meir@meirbank.com>
2024-11-17 00:33:13 +0700

    styling vaults cards

--- src/components/VaultCard.tsx
+++ src/components/VaultCard.tsx
@@ -13,7 +13,7 @@ export default function VaultCard({
   return (
     <div
       className={cn(
-        'h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl',
+        'min-h-[35vh] w-full max-w-[60vh] justify-self-center h-[35vh] col-span-1 bg-[#1f2324] border border-[#456DB5] rounded-xl',
         selectedVault && vault?.id === selectedVault?.id && 'border-2 border-white'
       )}
     >
