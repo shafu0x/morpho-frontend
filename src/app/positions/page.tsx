be371d16d7c6a371a569b3d019df5bddef727bc8
meirbank <meir@meirbank.com>
2024-11-16 12:05:39 +0700

    start my positions

--- src/app/positions/page.tsx
+++ src/app/positions/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+
+import ManagePositionsForm from '@/components/ManagePositionsForm';
+import MyPositions from '@/components/MyPositions';
+import { Separator } from '@/components/ui/separator';
+import { useState } from 'react';
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-white">Your positions</h1>
+    <div className="grid grid-cols-3">
+      <div className="col-span-1 px-8">
+        <ManagePositionsForm />
+      </div>
+      <div className="col-span-2 flex justify-between">
+        <Separator orientation="vertical" className="w-0.5" />
+        <div className="w-full px-8">
+          <MyPositions />
+        </div>
+      </div>
     </div>
   );
 }
